76ca53b07f1c7180de1565b2bf324d52a88ab240
Víctor Flamenco <vicflamenco@hotmail.com>
2017-12-21 22:46:22 +0100

    Refactor all code

---
+++ index.js
@@ -0,0 +1,31 @@
+const port = 8080;
+var messages = [
+    { id: 1, nickname: 'Bot', message: 'Welcome to chat' }
+];
+
+var express = require('express');
+var app = express();
+var httpServer = require('http').Server(app);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+httpServer.listen(port, () => {
+    console.log('Server listening port:' + port.toString() + '/');
+});
+
+var io = require('socket.io')(httpServer);
+
+io.on('connection', (socket) => {
+    console.log('New connection from: ' + socket.handshake.address);
+    socket.emit('messages', messages);
+
+    socket.on('add-message', function(data) {
+        messages.push(data);
+        console.log('New message from ' + socket.handshake.address);
+        io.sockets.emit('messages', messages);
+    });
+});
